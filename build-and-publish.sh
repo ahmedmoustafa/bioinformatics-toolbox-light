60249cd79753a6664fedb1263b7875f2f365428a
Ahmed Moustafa <amoustafa@aucegypt.edu>
2021-08-01 14:57:47 +0200

    Update build-and-publish.sh

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo docker build -t ghcr.io/ahmedmoustafa/bioinformatics-tools-light .
-# sudo docker run -v /data:/data -it ghcr.io/ahmedmoustafa/bioinformatics-tools-light /bin/bash
+sudo docker build -t ghcr.io/ahmedmoustafa/bioinformatics-toolbox-light .
+# sudo docker run -v /data:/data -it ghcr.io/ahmedmoustafa/bioinformatics-toolbox-light /bin/bash
 # ~/token.txt is a Github personal access tokens
 cat ~/token.txt | sudo docker login ghcr.io -u ahmedmoustafa --password-stdin
-sudo docker push ghcr.io/ahmedmoustafa/bioinformatics-tools-light
+sudo docker push ghcr.io/ahmedmoustafa/bioinformatics-toolbox-light
